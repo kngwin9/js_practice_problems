1bba4e32380190a7881c26a765252155f596862b
Kevin Nguyen <kngwin9@gmail.com>
2017-08-28 18:05:48 -0700

    adding row sum of odd numbers, need to complete

---
+++ sum_of_digits/main.js
@@ -0,0 +1,27 @@
+// iven the triangle of consecutive odd numbers:
+//
+//     1
+// 3     5
+// 7     9    11
+// 13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+//
+// rowSumOddNumbers(1); // 1
+// rowSumOddNumbers(2); // 3 + 5 = 8
+
+const rowSumOddNumbers = (n) => {
+    let result = 0;
+
+    for (let i = 0; i <= n.length - 1; i++){
+        console.log(i)
+    }
+
+
+    console.log(result);
+};
+
+// rowSumOddNumbers(1);
+rowSumOddNumbers(2);
+// rowSumOddNumbers(42);
