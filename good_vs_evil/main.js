0dfc49d8428edc26ee8cc8855fb24554f2307a22
Kevin Nguyen <kngwin9@gmail.com>
2017-09-05 15:41:52 -0700

    saving, finished

--- good_vs_evil/main.js
+++ good_vs_evil/main.js
@@ -39,104 +39,26 @@
 //
 // Return ""Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Evil eradicates all trace of Good" if evil wins, or "Battle Result: No victor on this battle field" if it ends in a tie.
 
-
 const goodVsEvil = (good, evil) => {
     let good_army = good.split(" ");
-    let good_result = 0;
-    let good_total = 0;
+    let good_total = good_army[0]*1 + good_army[1]*2 + good_army[2]*3 + good_army[3]*3 + good_army[4]*4 + good_army[5]*10;
     let evil_army = evil.split(" ");
-    let evil_result = 0;
-    let evil_total = 0;
-
-    for (let i = 0; i <= good_army.length - 1; i++) {
-        switch (good_army[i]) {
-
-            case good_army[0]:
-                good_result = Number(good_army[i]) * 1;
-                good_total += good_result;
-                break;
-
-            case good_army[1]:
-                good_result = Number(good_army[i]) * 2;
-                good_total += good_result;
-                break;
-
-            case good_army[2]:
-                good_result = Number(good_army[i]) * 3;
-                good_total += good_result;
-                break;
-
-            case good_army[3]:
-                good_result = Number(good_army[i]) * 3;
-                good_total += good_result;
-                break;
-
-            case good_army[4]:
-                good_result = Number(good_army[i]) * 4;
-                good_total += good_result;
-                break;
-
-            case good_army[5]:
-                good_result = Number(good_army[i]) * 10;
-                good_total += good_result;
-                break;
-        }
-    }
-
-    for (let j = 0; j <= evil_army.length - 1; j++) {
-
-        switch (evil_army[j]) {
-
-            case evil_army[0]:
-                evil_result = Number(evil_army[j]) * 1;
-                evil_total += evil_result;
-                break;
-
-            case evil_army[1]:
-                evil_result = Number(evil_army[j]) * 2;
-                evil_total += evil_result;
-                break;
-
-            case evil_army[2]:
-                evil_result += Number(evil_army[j]) * 2;
-                evil_total += evil_result;
-                break;
-
-            case evil_army[3]:
-                evil_result += Number(evil_army[j]) * 2;
-                evil_total += evil_result;
-                break;
-
-            case evil_army[4]:
-                evil_result += Number(evil_army[j]) * 3;
-                evil_total += evil_result;
-                break;
-
-            case evil_army[5]:
-                evil_result += Number(evil_army[j]) * 5;
-                evil_total += evil_result;
-                break;
-
-            case evil_army[6]:
-                evil_result += Number(evil_army[j]) * 10;
-                evil_total += evil_result;
-                break;
-        }
-    }
+    let evil_total = evil_army[0]*1 + evil_army[1]*2 + evil_army[2]*2 + evil_army[3]*2 + evil_army[4]*3 + evil_army[5]*5 + evil_army[6]*10;
     console.log("Good: " + good_total);
     console.log("Evil: " + evil_total);
-    // if (good_result === evil_result) {
-    //     console.log('Battle Result: No victor on this battle field');
-    //     return 'Battle Result: No victor on this battle field';
-    // }
-    // else if (good_result > evil_result) {
-    //     console.log('Battle Result: Good triumphs over Evil');
-    //     return 'Battle Result: Good triumphs over Evil';
-    // }
-    // else if (evil_result > good_result) {
-    //     console.log('Battle Result: Evil eradicates all trace of Good');
-    //     return 'Battle Result: Evil eradicates all trace of Good'
-    // }
+
+    if (good_total === evil_total) {
+        console.log('Battle Result: No victor on this battle field');
+        return 'Battle Result: No victor on this battle field';
+    }
+    else if (good_total > evil_total) {
+        console.log('Battle Result: Good triumphs over Evil');
+        return 'Battle Result: Good triumphs over Evil';
+    }
+    else if (evil_total > good_total) {
+        console.log('Battle Result: Evil eradicates all trace of Good');
+        return 'Battle Result: Evil eradicates all trace of Good'
+    }
 };
 
 goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1'); // 'Battle Result: Evil eradicates all trace of Good', 'Evil should win' );
